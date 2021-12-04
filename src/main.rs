3fa711e2044866cdd53f652671fac18918174f9c
Coda Hale <coda.hale@gmail.com>
2021-12-04 11:38:33 -0700

    Use clap for the CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
+use std::path::PathBuf;
 
 use anyhow::Result;
+use clap::{crate_description, crate_name, crate_version, AppSettings, Parser, ValueHint};
 
 use crate::site::Site;
 
@@ -8,14 +10,22 @@ mod latex;
 mod site;
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let enable_drafts = args.contains(&"--drafts".to_string());
-
-    let dir = &args[0];
-    let mut site = Site::load(dir, enable_drafts).expect("error loading site");
-    print!("Building {}...", dir);
+    let opts: Opts = Opts::parse();
+    let mut site = Site::load(&opts.dir, opts.drafts).expect("error loading site");
+    print!("Building {}...", &opts.dir.to_string_lossy());
     site.build()?;
     println!("OK!");
 
     Ok(())
+}
+
+#[derive(Debug, Parser)]
+#[clap(bin_name = crate_name!(), about = crate_description!(), version = crate_version!())]
+#[clap(setting = AppSettings::HelpRequired)]
+struct Opts {
+    #[clap(about = "The site directory", value_hint = ValueHint::DirPath)]
+    dir: PathBuf,
+
+    #[clap(long, about = "Include draft pages")]
+    drafts: bool,
 }
